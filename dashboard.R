8d56d91be0fd3b8b19f4b97ff19b5927ce4a6929
msaaddev <mrsaadirfan@gmail.com>
2019-11-30 11:11:44 +0500

    📦 NEW: sidebar_items

--- dashboard.R
+++ dashboard.R
@@ -2,11 +2,39 @@ library(shiny)
 library(shinydashboard)
 
 userInterface <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardHeader(title = "Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "dashboard", 
+                fluidRow(
+        box(
+          title = "Controls",
+          sliderInput("slider", "How much did you like my first attempt:", 1, 100, 50)
+        ),
+        
+        box(plotOutput("plot1", height = 250)),
+      ) 
+      )
+    )
+    
+  )
 )
 
-server <- function(input, output) { }
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
 
 shinyApp(userInterface, server)
